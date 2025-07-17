8c5d8641759182d6d299cfe4d4ef8644f2d81a75
sehn00 <sehn00@users.noreply.github.com>
2025-07-17 18:36:32 +0900

    Update gitignore

--- prep/src/format-specifer.c
+++ prep/src/format-specifer.c
@@ -3,9 +3,9 @@
 int main()
 {
     printf("%d\n", 10);
-    printf("%lf\n", 3.1415);
-    printf("%.1lf\n", 3.1415);
-    printf("%.10lf\n", 3.1415);
+    printf("%lf\n", 3.1415);        // double은 lf
+    printf("%.1lf\n", 3.1415);      // 소수점 첫째자리까지
+    printf("%.10lf\n", 3.1415);     // 소수점 열째자리까지
 
     printf("%d와 %d의 합은 %d입니다\n", 10, 20, 10 + 20);
     printf("%.1lf - %.1lf = %.1lf\n", 3.14, 1.2, 3.14 - 1.2);
