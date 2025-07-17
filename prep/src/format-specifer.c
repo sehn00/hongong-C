d61021e02de0d4914d260f31bb6dbc3b681cf17a
sehn00 <sehn00@users.noreply.github.com>
2025-07-17 18:04:33 +0900

    README 작성

---
+++ prep/src/format-specifer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n", 10);
+    printf("%lf\n", 3.1415);
+    printf("%.1lf\n", 3.1415);
+    printf("%.10lf\n", 3.1415);
+
+    printf("%d와 %d의 합은 %d입니다\n", 10, 20, 10 + 20);
+    printf("%.1lf - %.1lf = %.1lf\n", 3.14, 1.2, 3.14 - 1.2);
+    
+    return 0;
+}
