a8c13fa9af3e9aa473e081b8f7f7f79744ec2474
sehn00 <sehn00@users.noreply.github.com>
2025-07-21 04:42:14 +0900

    Update c02-src

---
+++ c02-arr-ptr/src/s02-array-loop.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, total = 0;
+    double avg;
+    int score[5];
+
+    int cnt = sizeof(score) / sizeof(score[0]);     // sizeof 연산자로 요소 개수 파악
+
+    printf("성적 %d개 입력: ", cnt);
+    for (i = 0; i < cnt; i++)
+    {
+        scanf("%d", &score[i]);     // 각 배열 요소에 성적 입력
+    }
+    
+    for (i = 0; i < cnt; i++)
+    {
+        total += score[i];
+    }
+    avg = total / (double)cnt;      // cnt의 타입캐스팅
+
+    for (i = 0; i < cnt; i++) 
+    {
+        printf("%4d", score[i]);
+    }
+
+    printf("\n평균: %.1lf\n", avg);
+
+    return 0;
+}
