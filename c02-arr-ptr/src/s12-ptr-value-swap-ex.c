31ceda6d6180974905dffa07374deeff377625f9
sehn00 <sehn00@users.noreply.github.com>
2025-07-21 10:16:37 +0900

    sleepy....

--- c02-arr-ptr/src/s12-ptr-value-swap-ex.c
+++ c02-arr-ptr/src/s12-ptr-value-swap-ex.c
@@ -22,3 +22,6 @@ void swap(int *pa, int *pb)
     *pa = *pb;
     *pb = temp;
 }
+
+// 포인터를 swap하지 않으면, 원본 a,b는 그대로 유지된다 (Call by Value)
+// 포인터로 접근하면, 메모리의 '실제 변수'에 접근하기 때문에 값이 직접 바뀜 (Call by Reference)
